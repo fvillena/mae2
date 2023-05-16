da176ab084b50626e55754dc1504220260fe75e1
Fabián Villena <villenafabian@gmail.com>
2023-05-16 22:32:35 +0000

    git safe

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,3 +1,5 @@
-git config core.fileMode false
+git config --global core.fileMode false
+git config --global core.autocrlf true
+git config --global --add safe.directory /workspaces/mae2
 pip3 install --upgrade pip 
 pip3 install --user -r requirements.txt

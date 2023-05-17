fd392cd78dd96e33f8620f94489f32ee6dac1890
Fabián Villena <villenafabian@gmail.com>
2023-05-17 00:47:09 +0000

    tf no cuda pip

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,5 @@
 git config --global core.autocrlf true
 git config --global --add safe.directory /workspaces/mae2
 git submodule update --init
-pip3 install --upgrade pip 
+pip3 install --user --upgrade pip 
 pip3 install --user -r requirements.txt

b8ac9baf42045094d9f4fc09f5caf5093b4c4f9a
Fabián Villena <villenafabian@gmail.com>
2023-06-01 00:21:53 +0000

    lfs oncreate

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -1,5 +1,6 @@
 git config --global core.autocrlf true
 git config --global --add safe.directory /workspaces/mae2
+git -C /workspaces/mae2/spanish_diagnostics lfs pull
 git submodule update --init
 pip3 install --user --upgrade pip 
 pip3 install --user -r requirements.txt
